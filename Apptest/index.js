fc48759d882d70cb9e60e887106dffb479ba477c
adisak <621463032@crru.ac.th>
2023-02-08 09:23:39 +0700

    adisak

--- Apptest/index.js
+++ Apptest/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 const db_name = path.join(__dirname, "data", "apptest.db");
 const db = new sqlite3.Database(db_name, err => {
   if (err) {
